78d3c265c3b3b491d8eed4939e7071a9d50e24f7
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-02-24 21:45:20 +0100

    Print tweets of aggregates

--- devobs.go
+++ devobs.go
@@ -10,9 +10,13 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 var username string
+var listAggregateNames bool
+var aggregateId int
+var aggregateTweetLimit int
 
 type Configuration struct {
 	User     string
@@ -20,6 +24,7 @@ type Configuration struct {
 	Database string
 }
 
+// See when init function is run at https://stackoverflow.com/q/24790175/282073
 func init() {
 	const (
 		defaultUsername = "fabpot"
@@ -29,17 +34,51 @@ func init() {
 	flag.StringVar(&username, "username", defaultUsername, usage)
 }
 
+func init() {
+	const (
+		defaultAggregateListing = false
+		usage = "List aggregate names"
+	)
+
+	flag.BoolVar(&listAggregateNames, "aggregate", defaultAggregateListing, usage)
+}
+
+func init() {
+	const (
+		defaultAggregateId = 0
+		usage = "The id of an aggregate, which tweets are to be printed out"
+		defaultTweetLimit = 1000
+		defaultAggregateTweetLimitUsage = "Maximum tweets be collected"
+	)
+
+	flag.IntVar(&aggregateId, "aggregate-id", defaultAggregateId, usage)
+	flag.IntVar(&aggregateTweetLimit, "aggregate-tweet-limit", defaultTweetLimit, defaultAggregateTweetLimitUsage)
+}
+
 func main() {
 	flag.Parse()
 
 	db := connectToMySqlDatabase()
 
-	selectTweetsOfUserWithUsername(username, db)
-	countTweetsOfUser(username, db)
-
 	// "defer" keyword is described at https://tour.golang.org/flowcontrol/12
 	defer db.Close()
+
+	if aggregateId != 0 {
+		selectTweetsOfAggregate(aggregateId, db)
+
+		return
+	}
+
+	if listAggregateNames {
+		selectAggregates(db)
+
+		return
+	}
+
+	selectTweetsOfUser(username, db)
+	countTweetsOfUser(username, db)
 }
+
 func connectToMySqlDatabase() *sql.DB {
 	err, configuration := parseConfiguration()
 
@@ -49,6 +88,7 @@ func connectToMySqlDatabase() *sql.DB {
 
 	return db
 }
+
 func parseConfiguration() (error, Configuration) {
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	handleError(err)
@@ -61,31 +101,9 @@ func parseConfiguration() (error, Configuration) {
 	return err, configuration
 }
 
-func countTweetsOfUser(username string, db *sql.DB) {
-	countTweets, err := db.Prepare(`SELECT count(*) as Count ` +
-		`FROM weaving_twitter_user_stream ` +
-		`WHERE ust_full_name = ?`)
-	var tweetCount int
-	row := countTweets.QueryRow(username)
-	err = row.Scan(&tweetCount)
-	handleError(err)
-	fmt.Printf(`%d tweets have been collected for "%s"`+"\n", tweetCount, username)
-}
+func selectAggregates(db *sql.DB) {
+	rows, err := db.Query(`SELECT id as Id, name as Name FROM weaving_aggregate`)
 
-func selectTweetsOfUserWithUsername(username string, db *sql.DB) {
-	selectTweets, err := db.Prepare(
-		`SELECT 
-			ust_full_name as Username,
-			ust_text as Tweet,
-			ust_api_document as "API source",
-			CONCAT("https://twitter.com/", ust_full_name, "/status/", ust_status_id) as URL,
-			ust_created_at as "Publication date" ` +
-			`FROM weaving_twitter_user_stream ` +
-			`WHERE ust_full_name = ? ` +
-			`ORDER BY ust_created_at DESC`)
-	handleError(err)
-
-	rows, err := selectTweets.Query(username)
 	columns, err := rows.Columns()
 	handleError(err)
 	values := make([]sql.RawBytes, len(columns))
@@ -93,12 +111,95 @@ func selectTweetsOfUserWithUsername(username string, db *sql.DB) {
 	for i := range values {
 		scanArgs[i] = &values[i]
 	}
+
+	for rows.Next() {
+		err = rows.Scan(scanArgs...)
+		handleError(err)
+
+		var value string
+		for i, col := range values {
+			value = string(col)
+			fmt.Printf("%s", strings.Replace(value, `user :: `, ``, -1))
+
+			if i == 0 {
+				fmt.Printf("\t")
+
+				continue
+			}
+
+			fmt.Printf("\n")
+		}
+	}
+}
+
+func countTweetsOfUser(username string, db *sql.DB) {
+	countTweets, err := db.Prepare(`
+		SELECT count(*) as Count 
+		FROM weaving_twitter_user_stream 
+		WHERE ust_full_name = ?`)
+	var tweetCount int
+	row := countTweets.QueryRow(username)
+	err = row.Scan(&tweetCount)
+	handleError(err)
+	fmt.Printf(`%d tweets have been collected for "%s"`+"\n", tweetCount, username)
+}
+
+func selectTweetsOfUser(username string, db *sql.DB) {
+	selectTweets, err := db.Prepare(`
+			SELECT 
+			ust_full_name as Username,
+			ust_text as Tweet,
+			ust_api_document as "API source",
+			CONCAT("https://twitter.com/", ust_full_name, "/status/", ust_status_id) as URL,
+			ust_created_at as "Publication date"
+			FROM weaving_twitter_user_stream 
+			WHERE ust_full_name = ? 
+			ORDER BY ust_created_at DESC`)
+	handleError(err)
+
+	rows, err := selectTweets.Query(username)
+	handleError(err)
+
+	printTweets(rows, db)
+}
+
+func selectTweetsOfAggregate(aggregateId int, db *sql.DB) {
+	selectTweets, err := db.Prepare(`
+			SELECT 
+			ust_full_name as Username,
+			ust_text as Tweet,
+			ust_api_document as "API source",
+			CONCAT("https://twitter.com/", ust_full_name, "/status/", ust_status_id) as URL,
+			ust_created_at as "Publication date"
+			FROM weaving_status_aggregate sa, weaving_twitter_user_stream s
+			WHERE s.ust_id = sa.status_id 
+			AND sa.aggregate_id = ? 
+			AND DATE(s.ust_created_at) >= SUBDATE(DATE(NOW()), 365)
+			ORDER BY sa.status_id DESC LIMIT ?`)
+	handleError(err)
+
+	rows, err := selectTweets.Query(aggregateId, aggregateTweetLimit)
+	handleError(err)
+
+	printTweets(rows, db)
+}
+
+func printTweets(rows *sql.Rows, db *sql.DB) {
 	type Message struct {
 		Text string `json:text`
 		Retweet_count  int `json:retweet_count`
 		Favorite_count int `json:favorite_count`
 	}
 	var decodedApiDocument Message
+
+	columns, err := rows.Columns()
+	handleError(err)
+	values := make([]sql.RawBytes, len(columns))
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
 	for rows.Next() {
 		err = rows.Scan(scanArgs...)
 		handleError(err)
